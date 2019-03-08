d82f35c1999f0bd8ffcc569a08d7a94e9e390aec
Alex Baryzhikov <aleksiarts@gmail.com>
2019-03-08 17:15:10 +0300

    sort

---
+++ Haskell/sort.hs
@@ -0,0 +1,22 @@
+-- variant 1
+
+sort :: Ord a => [a] -> [a]
+sort [] = []
+sort (x : xs) = (sort ys) ++ [x] ++ (sort zs)
+    where ys = [y | y <- xs, y <= x]
+          zs = [z | z <- xs, z > x]
+
+
+-- variant 2 (single pass)
+
+sort1 :: Ord a => [a] -> [a]
+sort1 [] = []
+sort1 (x : xs) = (sort as) ++ bs ++ (sort cs)
+    where (as, bs, cs) = foldr (insert x) ([], [], []) (x : xs)
+
+
+insert :: Ord a => a -> a -> ([a], [a], [a]) -> ([a], [a], [a])
+insert x i (as, bs, cs)
+    | i < x     = (i : as, bs, cs)
+    | i == x    = (as , i : bs, cs)
+    | otherwise = (as, bs, i : cs)
