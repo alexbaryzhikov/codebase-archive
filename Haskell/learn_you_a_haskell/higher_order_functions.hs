2a31c277f288e2aa2e26668e9d80401bbc74b4db
Alex Baryzhikov <aleksiarts@gmail.com>
2020-01-30 23:56:28 +0300

    feat: higher order functions

--- Haskell/learn_you_a_haskell/higher_order_functions.hs
+++ Haskell/learn_you_a_haskell/higher_order_functions.hs
@@ -9,6 +9,23 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 flip' :: (a -> b -> c) -> b -> a -> c
 flip' f x y = f y x
 
+largestDivisible :: Int
+largestDivisible = head $ filter p [100000,99999..]
+    where p x = mod x 3829 == 0
+
+sumOfSquaredOdds :: Int
+sumOfSquaredOdds = sum $ takeWhile (< 10000) $ filter odd $ map (^2) [1..]
+
+collatzChain :: Int -> [Int]
+collatzChain x = x : next
+    where next
+            | x == 1 = [] 
+            | even x = collatzChain (div x 2)
+            | otherwise = collatzChain (x * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length $ filter (\xs -> length xs > 15) $ map collatzChain [1..100]
+
 main :: IO ()
 main = do
     print $ applyTwice (+3) 1 -- 7
@@ -20,3 +37,13 @@ main = do
 
     print $ flip' zip "hello" [1..] -- [(1,'h'),(2,'e'),(3,'l'),(4,'l'),(5,'o')]
     print $ zipWith' (flip' div) [2,2..] [1..5] -- [0,1,1,2,2]
+
+    print $ map (+3) [1,2,3,4] -- [4,5,6,7]
+    print $ filter (>3) [1,2,3,4] -- [4]
+    print $ filter (not.null) [[1,2],[],[],[3]] -- [[1,2],[3]]
+
+    print largestDivisible -- 99554
+    print sumOfSquaredOdds -- 166650
+
+    print $ collatzChain 13 -- [13,40,20,10,5,16,8,4,2,1]
+    print numLongChains -- 66
