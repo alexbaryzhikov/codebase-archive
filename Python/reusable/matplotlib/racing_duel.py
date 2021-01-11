c78c189002a26296a552f30078578cc0cf72e426
Alexey Baryzhikov <aleksiarts@gmail.com>
2021-01-11 18:47:50 +0300

    Fix plotting in racing_duel.py

--- Python/reusable/matplotlib/racing_duel.py
+++ Python/reusable/matplotlib/racing_duel.py
@@ -16,42 +16,40 @@ from numpy import arange
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 import matplotlib.pyplot as plt
 
-def map2(fn, A, B):
-    """Map fn to corresponding elements of 2D arrays A and B."""
-    return [list(map(fn, Arow, Brow)) for (Arow, Brow) in zip(A, B)]
 
 def score(A, B):
     """Compute the outcome of the race for player A, given A and B to be speeds of the cars."""
-    assert 10 <= A <= 100 and 10 <= B <= 100 # you can't get too slow or too fast
+    assert 10 <= A <= 100 and 10 <= B <= 100  # you can't get too slow or too fast
     trackA = 100 - B
     trackB = 100 - A
     tA = trackA/A
     tB = trackB/B
     return tB - tA
 
-## Graph the space of possible scores for A
 
+# Graph the space of possible scores for A
 speeds = arange(10, 101)
 A, B = np.meshgrid(speeds, speeds)
+S = np.array([list(map(score, Arow, Brow)) for (Arow, Brow) in zip(A, B)])
+
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(1, 1, 1, projection='3d')
 ax.set_xlabel('Speed of A')
 ax.set_ylabel('Speed of B')
 ax.set_zlabel('Race Score for A')
-ax.plot_surface(A, B, map2(score, A, B));
-plt.show()
-# plt.savefig('racing_duel.png')
+ax.plot_surface(A, B, S)
+plt.savefig('racing_duel.png')
 plt.close(fig)
 
-## Maximum score for A
-print("Max score for A:", max([score(A, B), A, B] for A in speeds for B in speeds))
+# Maximum score for A
+print("Max score for A:",
+      max([score(A, B), A, B] for A in speeds for B in speeds))
 
-## If player B gets to respond with a speed choice, with full knowledge of A's choice, what
-## speed should A choose to maximize the score?
+# If player B gets to respond with a speed choice, with full knowledge of A's choice, what
+# speed should A choose to maximize the score?
 print("Max score for A if B responds:",
-    max(min([score(A, B), A, B] for B in speeds) for A in speeds))
+      max(min([score(A, B), A, B] for B in speeds) for A in speeds))
 
-## What if B chooses a speed first, and then A responds?
-print("Max score for A if A responds:",
-    min(max([score(A, B), A, B] for A in speeds) for B in speeds))
-
+# What if B chooses a speed first, and then A responds?
+print("Max score for B if A responds:",
+      min(max([score(A, B), A, B] for A in speeds) for B in speeds))
